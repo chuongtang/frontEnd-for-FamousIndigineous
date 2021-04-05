98ca28e1e5417575c9a537a7081b06ae6514a788
ChuongTang <chuong.tang@aol.com>
2021-04-04 20:54:26 -0600

    Perfect 1

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Famous Aborigines</Typography>
+        <Typography className={classes.heading} variant="h3" align="center">Famous Aborigines</Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
--- src/styles.js
+++ src/styles.js
@@ -10,9 +10,9 @@ export default makeStyles(() => ({
     alignItems: 'center',
   },
   heading: {
-    color: 'rgba(0,183,255, 1)',
+    color: '#FF5D33',
   },
   image: {
-    marginLeft: '15px',
+    marginLeft: '55px',
   },
 }));
