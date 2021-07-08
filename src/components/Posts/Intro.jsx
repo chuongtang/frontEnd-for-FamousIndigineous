5a02a7561d79d5c18fd161ccfd2d0cea71434ae2
ChuongTang <chuong.tang@aol.com>
2021-07-07 19:20:48 -0600

    completed upgrade

---
+++ src/components/Posts/Intro.jsx
@@ -0,0 +1,13 @@
+import './Intro.css';
+
+
+const Intro = () => {
+  
+  return ( 
+   
+<h6 className='introText'>The first day of summer, June 21, marks National Indigenous Peoples Day, a day to recognize and celebrate First Nations people, Métis, and Inuit in Canada.
+This project was built to help my kids learn more about Aboriginal cultures, and celebrate the greatness of Indigenous Heros that they find on the internet.</h6>
+   );
+}
+ 
+export default Intro;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,13 +4,19 @@ import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
 import useStyles from './styles';
+import Intro from './Intro';
 
 const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
   return (
-    !posts.length ? <CircularProgress /> : (
+    !posts.length ? 
+    <>
+    <CircularProgress /> 
+    <Intro />
+    </>
+    : (
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
         {posts.map((post) => (
           <Grid key={post._id} item xs={12} sm={6} md={6}>
