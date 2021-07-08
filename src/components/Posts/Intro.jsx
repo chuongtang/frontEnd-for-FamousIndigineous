7ad48f89e63af5f9ddabac31c0577069bf6c4822
ChuongTang <chuong.tang@aol.com>
2021-07-07 23:24:58 -0600

    fixed loading text

--- src/components/Posts/Intro.jsx
+++ src/components/Posts/Intro.jsx
@@ -5,8 +5,7 @@ const Intro = () => {
   
   return ( 
    
-<h6 className='introText'>The first day of summer, June 21, marks National Indigenous Peoples Day, a day to recognize and celebrate First Nations people, Métis, and Inuit in Canada.
-This project was built to help my kids learn more about Aboriginal cultures, and celebrate the greatness of Indigenous Heros that they find on the internet.</h6>
+<h6 className='introText'>Celebrating the heritage, diverse cultures and outstanding achievements of 🍁 Indigenous People</h6>
    );
 }
  
