db74228965bc553111faf2fce7aeafbacea46bbb
ChuongTang <chuong.tang@aol.com>
2021-07-08 23:52:11 -0600

    add pointer

--- src/components/Posts/Intro.jsx
+++ src/components/Posts/Intro.jsx
@@ -2,11 +2,11 @@ import './Intro.css';
 
 
 const Intro = () => {
-  
-  return ( 
-   
-<h6 className='introText'>Who is your 🍁 Hero? Do you have one to share?</h6>
-   );
+
+  return (
+
+    <h6 className='introText'>Who is your 🍁 Hero? Do you have one to share? Add a HERO card into this awesome site ➥</h6>
+  );
 }
- 
+
 export default Intro;
