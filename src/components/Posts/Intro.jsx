947213e4741b02e860fae622b1558b13807269ba
chuongtang <chuong.tang@aol.com>
2021-08-21 17:08:45 -0600

    prior to adding OAuth

--- src/components/Posts/Intro.jsx
+++ src/components/Posts/Intro.jsx
@@ -5,7 +5,7 @@ const Intro = () => {
 
   return (
 
-    <h6 className='introText'>Who is your 🍁 Hero? Do you have one to share? Add a HERO card into this awesome site ➥</h6>
+    <h6 className='introText'>Who is your 🍁 Hero?  Add a HERO card into this awesome site ➥</h6>
   );
 }
 
