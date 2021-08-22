84f77eaf388ef58e410c4708615c6033588dc499
chuongtang <chuong.tang@aol.com>
2021-08-22 10:32:17 -0600

    added OAuth package and components

---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+
+  const { loginWithRedirect } = useAuth0();
+
+  return <button
+    variant="contained"
+    color="secondary"
+    size="small"
+    onClick={() => loginWithRedirect()}>
+    Log In
+  </button>;
+};
+
+
+export default LoginButton;
--- src/index.js
+++ src/index.js
@@ -8,12 +8,21 @@ import { reducers } from './reducers';
 import App from './App';
 import './index.css';
 
+import { Auth0Provider } from "@auth0/auth0-react";
+
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
- 
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Auth0Provider
+    domain={process.env.APP_AUTH0_DOMAIN}
+    clientId={process.env.APP_CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
+
+    <Provider store={store}>
+      <App />
+    </Provider>,
+
+  </Auth0Provider>,
   document.getElementById('root'),
 );
