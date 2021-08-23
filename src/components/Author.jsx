0b1c2a0b3bcdaf95649f9e2cee338409272ca515
chuongtang <chuong.tang@aol.com>
2021-08-23 08:34:47 -0600

    added link to portfolio

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -5,6 +5,7 @@ const Author = () => {
     <div>
       {/* <h6>Built by  <img src="./Forms/FaviconCLear.png" alt="Developer" /></h6> */}
       <h6 style={{fontStyle: 'italic', textAlign:'center'}}>  Built by  <img style={{maxHeight: '1.5rem'}} src={FaviconClear} alt="CT_Favicon" />, a Full Stack Developer</h6>
+      <a href="https://chuongtang.github.io/aboutme" target="_blank" rel="noopener noreferrer">More about me.</a>
     </div>
    
    );
